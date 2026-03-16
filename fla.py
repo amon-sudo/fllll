f6acf45d57dd1a0a6e613ffb1100b7d08d17930a
amon-sudo <amonogino@gmail.com>
2026-03-16 23:51:15 +0300

    wqa

--- fla.py
+++ fla.py
@@ -2,13 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return "<h1>this is home</h1>"
+    return "Home Page"
 
+@app.route("/test")
+def test():
+    return "<h1>Test</h1>"
 
-
-
+print(app.url_map)
 
 if __name__ == "__main__":
     app.run(debug=True)
