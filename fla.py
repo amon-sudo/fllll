f905605271823e1fc3ca254022055ff316c7238c
amon-sudo <amonogino@gmail.com>
2026-03-16 23:40:39 +0300

    asqa

---
+++ fla.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return "<h1>this is home</h1>"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
